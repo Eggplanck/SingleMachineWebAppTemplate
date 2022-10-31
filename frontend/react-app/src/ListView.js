b3d68541772e28258bc7e36a7a599b781edfdf26
Eggplanck <cptsym496@yahoo.co.jp>
2022-11-01 00:03:46 +0900

    make the looks of the App

---
+++ frontend/react-app/src/ListView.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Header from './Header';
+import TitleCard from './TitleCard';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+
+function ListView() {
+  return (
+    <Box>
+      <Header showButton={true} />
+      <Stack spacing={1} alignItems="center" sx={{ mt: 2 }}>
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <TitleCard />
+        <Box sx={{ height: 100 }} />
+      </Stack>
+      <Fab color='primary' sx={{ position: 'fixed', right: 24, bottom: 24 }}>
+        <AddIcon />
+      </Fab>
+    </Box>
+  );
+}
+
+export default ListView;
