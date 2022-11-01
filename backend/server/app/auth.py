e27b29f8bed6b964b834d697fb1becec76990011
Eggplanck <cptsym496@yahoo.co.jp>
2022-11-01 19:03:30 +0900

    exception handling

--- backend/server/app/auth.py
+++ backend/server/app/auth.py
@@ -2,12 +2,13 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, Depends, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 import datetime
 
 from . import crud
 from .database import get_db
+from .exceptions import AuthenticationException
 
 
 SECRET_KEY = "aba93fb5652c37f20da66725a7a107ec36d98f7e6a04faacfa2892bec7fce9b0"
@@ -44,19 +45,14 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
+            raise AuthenticationException(detail="Could not validate credentials")
     except JWTError:
-        raise credentials_exception
+        raise AuthenticationException(detail="Could not validate credentials")
     user = crud.get_user_by_username(db, username)
     if user is None:
-        raise credentials_exception
+        raise AuthenticationException(detail="Could not validate credentials")
     return user
