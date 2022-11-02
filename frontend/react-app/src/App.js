49e801b9d79efea2831385e694d6122398d5f98a
Eggplanck <cptsym496@yahoo.co.jp>
2022-11-02 19:05:10 +0900

    connect frontend to auth api

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import DetailView from './DetailView';
 import ListView from './ListView';
@@ -8,7 +8,31 @@ import SignUpView from './SignUpView';
 import './App.css';
 
 function App() {
-  const view = <DetailView />;
+  const [viewMode, setViewMode] = useState('SignIn');
+
+  const changeView = (viewMode) => {
+    setViewMode(viewMode);
+  };
+
+  const modeToView = (viewMode) => {
+    switch (viewMode) {
+      case 'List':
+        return <ListView changeView={changeView} />;
+      case 'Detail':
+        return <DetailView changeView={changeView} />;
+      case 'Edit':
+        return <EditView changeView={changeView} />;
+      case 'SignIn':
+        return <SignInView changeView={changeView} />;
+      case 'SignUp':
+        return <SignUpView changeView={changeView} />;
+      default:
+        return <SignInView changeView={changeView} />;
+    }
+  };
+
+  const view = modeToView(viewMode);
+
   return (
     <Box className='App'>
       {view}
